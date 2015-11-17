55416c43609775b146f3c4c572531c298a78a411
Claire Hu <hum085@newschool.edu>
2015-11-16 19:16:40 -0500

    no message

--- Space Shooter/Space Shooter/Assets/RandomDirection.cs
+++ Space Shooter/Space Shooter/Assets/RandomDirection.cs
@@ -5,7 +5,9 @@ public class RandomDirection : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-		GetComponent<Rigidbody> ().AddForce (Random.insideUnitCircle * 1000);
+		//GetComponent<Rigidbody> ().AddForce (Random.insideUnitCircle * 1000);
+
+		GetComponent<Rigidbody> ().AddForce ( new Vector3 ( Random.Range(-1000, 1000), Random.Range(-1000, 1000), 0 ));
 	}
 	
 	// Update is called once per frame
